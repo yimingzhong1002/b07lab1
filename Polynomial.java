57d6ced52280b60695e9ede9a6c78873a6c52c39
yimingzhong1002 <103668710+yimingzhong1002@users.noreply.github.com>
2023-09-25 19:24:28 -0400

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,34 +1,43 @@
 public class Polynomial{
-    double[] coefficients = new double[100];
-    // initialize a class Polynomial, which contains solely primitive, numeric type, double
+    double[] coefficients;
 
     public Polynomial(){
-    // create this no-argument constructor to return 0.0 when passing down n/a
+    coefficients = new double[1];
+    coefficients[0] = 0.0;
     }
 
-    public Polynomial(double arr[]){
+    public Polynomial(double[] arr){
+        coefficients = new double[arr.length];
         for(int i=0; i<arr.length; i++){
             coefficients[i]=arr[i];
         }
     }
 
-    public Polynomial add(Polynomial function){
-    // 'add' method
-        Polynomial result = new Polynomial(); 
-    // Create a new Polynomial to store the result
-        for (int i = 0; i < 100; i++) {
-            result.coefficients[i] = this.coefficients[i] + function.coefficients[i];
+    public Polynomial add(Polynomial function) {
+        int max_len = Math.max(this.coefficients.length, function.coefficients.length);
+        int min_len = Math.min(this.coefficients.length, function.coefficients.length);
+        Polynomial result = new Polynomial(new double[max_len]);
+        
+        for(int i=0;i<max_len;i++){
+            if(i<min_len){
+                result.coefficients[i] = this.coefficients[i] + function.coefficients[i];
+            }
+            else
+                if(max_len == this.coefficients.length)
+                    result.coefficients[i] = coefficients[i];
+                else
+                    result.coefficients[i] = function.coefficients[i];
         }
-        return result; // Return the result Polynomial
-    }
+        return result;
+    }    
 
-    public double evaluate(double x){
+    public double evaluate(double x) {
         double sum = 0;
-        for(int i=0; i<100; i++){
-            sum = sum + coefficients[i]*Math.pow(x,i);
+        for (int i = 0; i < coefficients.length; i++) {
+            sum += coefficients[i] * Math.pow(x, i);
         }
         return sum;
-    }
+    }    
 
     public boolean hasRoot(double root){
         double result = evaluate(root);
